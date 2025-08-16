0415aa27476256722dcdeaf024cfa0f41bc7cb9f
DIGPALSINGHUMATH <digpalsinghumath@gmail.com>
2025-08-16 11:11:00 +0530

    backend start

---
+++ backend/Blood Donor Network/Blood-Donor-Network/src/main/java/com/Blood_Donor_Network/Blood/Donor/Network/BloodDonorNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.Blood_Donor_Network.Blood.Donor.Network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodDonorNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodDonorNetworkApplication.class, args);
+	}
+
+}
---
+++ backend/Blood Donor Network/Blood-Donor-Network/src/test/java/com/Blood_Donor_Network/Blood/Donor/Network/BloodDonorNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Blood_Donor_Network.Blood.Donor.Network;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodDonorNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
